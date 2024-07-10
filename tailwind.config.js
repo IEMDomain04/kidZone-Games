ad4a0a7697d5d071df5cc147f74bbab68d993af1
unknown <Ggwepq>
2024-07-10 23:59:27 +0800

    chore: Tailwind related changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                'kid-blue': 'rgb(17,0,124)',
+            },
+            fontFamily: {
+                lilita: ['Lilita One']
+            }
+
+        },
+    },
+    plugins: [],
 }
 
